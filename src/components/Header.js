b5d5a818a8dab425fe0419d713c442a343f30b71
Niranjan5601 <lavanyninja@gmail.com>
2024-03-14 20:32:32 +0530

    Login and signUp page

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
+       <img 
+       className='w-44'
+       alt="logo" src='https://assets.nflxext.com/ffe/siteui/acquisition/home/nflxlogo.png' />
+    </div>
+  )
+}
+
+export default Header
